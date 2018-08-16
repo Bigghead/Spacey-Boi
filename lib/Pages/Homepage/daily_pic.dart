ab95ccc247e4b2b16215102a8184916a2531e2a9
Bigghead <shavenchew@yahoo.com>
2018-08-15 18:08:42 -0700

    fetching the other kinds of images

--- lib/Pages/Homepage/daily_pic.dart
+++ lib/Pages/Homepage/daily_pic.dart
@@ -1,14 +1,21 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 import '../../keys.dart';
 import './fake_data.dart';
 
 
 class DailyPic extends StatefulWidget {
+
+  final String widgetText;
+  final String pictureType;
+
+  DailyPic( { @required this.widgetText, @required this.pictureType } );
 
   @override
     State<StatefulWidget> createState() {
@@ -20,15 +27,42 @@ class DailyPic extends StatefulWidget {
 
 class _DailyPicState extends State<DailyPic> {
 
-  final String _url = 'https://api.nasa.gov/planetary/apod?api_key=${api_key}';
+  // final Map _url = {
+  //   'daily': 'https://api.nasa.gov/planetary/apod?api_key=${api_key}',
+  //   'gallery': 'https://api.nasa.gov/planetary/apod?date=${_getYesterdayDate()}api_key=${api_key}'
+  // };
+
+  String _url = '';
   Map _dailyData = {};
+  String _widgetText;
+  String _pictureType;
 
 
   @override
   initState(){
-    // _getDailyData();
-    _dailyData = fakeData;
+    _getDailyData();
+    // _dailyData   = fakeData;
+    _widgetText  = widget.widgetText;
+    _pictureType = widget.pictureType;
+    switch( _pictureType ) {
+      case 'daily':
+        _url = 'https://api.nasa.gov/planetary/apod?api_key=${api_key}';
+        return;
+      case 'gallery':
+        _url = 'https://api.nasa.gov/planetary/apod?date=${_getPicture(1)}&api_key=${api_key}';
+        return;
+      case 'random':
+        _url = 'https://api.nasa.gov/planetary/apod?date=${_getPicture(Random().nextInt(30))}&api_key=${api_key}';
+    }
     super.initState();
+  }
+
+  String _getPicture( int day) {
+    var now = new DateTime.now();
+    var yesterday = DateTime(now.year, now.month, now.day - (day) );
+    var formatter = new DateFormat('yyyy-MM-dd');
+    String formattedDate = formatter.format(yesterday);
+    return formattedDate;
   }
 
   Future<Null> _getDailyData() async {
@@ -48,14 +82,18 @@ class _DailyPicState extends State<DailyPic> {
     Widget build(BuildContext context) {
       // TODO: implement build
       return Container(
-        child: _dailyData['url'] != null 
-            ? Column(
+        child: Column(
               children: <Widget>[
                 Expanded(
-                  child: Image(
-                    fit: BoxFit.fitHeight,
-                    image: NetworkImage(_dailyData['url'])
-                  ),
+                  child: _dailyData['url'] != null
+                        ? FadeInImage(
+                          placeholder: NetworkImage('https://imgplaceholder.com/420x320/fbf3f3/333333/fa-image'),
+                          fit: BoxFit.fitHeight,
+                          image: NetworkImage(_dailyData['url'])
+                        )
+                        : Image(
+                          image: NetworkImage('https://imgplaceholder.com/420x320/fbf3f3/333333/fa-image'),
+                        )
                 ),
                 Container(
                   alignment: AlignmentDirectional.center,
@@ -64,11 +102,10 @@ class _DailyPicState extends State<DailyPic> {
                   decoration: BoxDecoration(
                     color: Colors.black.withOpacity(0.9)
                   ),
-                  child: Text('Gallery', style: TextStyle(color: Colors.white),),
+                  child: Text(_widgetText, style: TextStyle(color: Colors.white),),
                 )
               ],
             )
-            : Text('Meep')
       );
     }
 }
--- lib/Pages/Homepage/homepage.dart
+++ lib/Pages/Homepage/homepage.dart
@@ -19,16 +19,16 @@ class HomePage extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-              Flexible( flex: 2, child: DailyPic(),),
+              Flexible( flex: 2, child: DailyPic(widgetText: 'Picture of the day', pictureType: 'daily',),),
               Flexible(
                 flex: 1,
                 child:Row(
                 children: <Widget>[
                   Expanded(
-                    child: DailyPic(),
+                    child: DailyPic(widgetText: 'Gallery', pictureType: 'gallery',),
                   ),
                   Expanded(
-                    child: DailyPic(),
+                    child: DailyPic(widgetText: 'Random', pictureType: 'random',),
                   )
                 ],
               )),
