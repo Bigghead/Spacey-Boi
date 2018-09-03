82612b7ed351c1cfe1a204bb76a2686e976a8fa3
Bigghead <shavenchew@yahoo.com>
2018-09-03 15:13:13 -0700

    update sidebar to capture date value

--- lib/UI/side_drawer.dart
+++ lib/UI/side_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 
 import '../Pages/Homepage/homepage.dart';
 import '../Pages/GalleryViewPage/gallery_view_page.dart';
@@ -63,7 +65,33 @@ class SideDrawer extends StatelessWidget {
                 Container(
                   margin: EdgeInsets.only(top: 30.0),
                   child: Column(
-                    children: _buildDrawerChildren(context)
+                    children: List.from(_buildDrawerChildren(context))..addAll([
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            showDatePicker(
+                                  context: context,
+                                  initialDate: new DateTime.now(),
+                                  firstDate:
+                                      new DateTime.now().subtract(new Duration(days: 30)),
+                                  lastDate: new DateTime.now().add(new Duration(days: 30)),
+                            ).then((date) => print(DateFormat('yyyy-MM-dd').format(date)));
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Expanded(
+                                child: Icon(Icons.date_range, color: Colors.white,),
+                              ),
+                              Expanded(
+                                child: Text('Date', style: TextStyle(color: Colors.white),),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ])
                   ),
                 )
               ],
